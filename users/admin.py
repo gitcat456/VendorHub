61ec2e1f8683e7c182ca8e33a67a9f20586affb2
gitcat456 <okothdenzel65@gmail.com>
2025-12-04 12:26:01 +0300

    registers user model to the admin site

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['id', 'username', 'email', 'phone_number', 'role']
+    list_filter = ['role']
+    search_fields = ['username', 'email']
+    list_editable = ['role']  
+
+admin.site.register(User, UserAdmin)
