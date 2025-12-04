d50a77843e074d02a11e8124ea98257e7987117e
gitcat456 <okothdenzel65@gmail.com>
2025-12-04 12:22:42 +0300

    creates a custom user model for my app

--- users/models.py
+++ users/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('admin', 'Admin'),
+        ('vendor', 'Vendor'),
+    )
+    
+    phone_number = models.CharField(max_length=15)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='vendor')
+    
+    def __str__(self):
+        return self.username
+    
+    def is_admin(self):
+        return self.role=='admin'
+    
+    def is_vendor(self):
+        return self.role=='vendor'
+    
+    
