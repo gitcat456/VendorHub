889cae0fb27ec41eced2c9a3c1345e339b085090
gitcat456 <okothdenzel65@gmail.com>
2025-12-05 07:16:53 +0300

    creates viewset classes to handle CRUD on the models

--- products/views.py
+++ products/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from rest_framework import viewsets 
+from .serializers import VendorProfileSerializer, CategorySerializer, ProductImageSerializer, ProductSerializer
+from .models import VendorProfile, Category, Product, ProductImage
 
-# Create your views here.
+class VendorProfileViewSet(viewsets.ModelViewSet):  
+    queryset = VendorProfile.objects.all()
+    serializer_class = VendorProfileSerializer
+
+class CategoryViewSet(viewsets.ModelViewSet):  
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class ProductViewSet(viewsets.ModelViewSet): 
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    
+    def get_queryset(self):
+        queryset = Product.objects.all()
+        vendor_id = self.request.query_params.get('vendor', None)
+        if vendor_id:
+            queryset = queryset.filter(vendor_id=vendor_id)
+        return queryset
+
+class ProductImageViewSet(viewsets.ModelViewSet):  
+    queryset = ProductImage.objects.all()
+    serializer_class = ProductImageSerializer
