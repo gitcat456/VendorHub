6cb1d9e2f2ff0760a35feeb29b161589931ed0c7
gitcat456 <okothdenzel65@gmail.com>
2025-12-05 07:17:33 +0300

    routing structure for tha api endpoints

---
+++ products/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import VendorProfileViewSet, CategoryViewSet, ProductViewSet, ProductImageViewSet
+
+router = DefaultRouter()
+router.register(r'vendor-profiles', VendorProfileViewSet)
+router.register(r'categories', CategoryViewSet)
+router.register(r'products', ProductViewSet)
+router.register(r'product-images', ProductImageViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
