a7ebc7845a7bfd6157955b73608e826a3ee1c755
gitcat456 <okothdenzel65@gmail.com>
2025-12-20 20:53:12 +0300

    creates register api class

--- users/views.py
+++ users/views.py
@@ -1,21 +1,35 @@
-from django.shortcuts import render
-from .forms import CustomUserCreationForm
-from django.views.generic import CreateView, DetailView
-from django.urls import reverse_lazy
-from django.contrib.auth.mixins import LoginRequiredMixin 
-from .models import User
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import RegisterSerializer
 
-class SignUpView(CreateView):
-    form_class = CustomUserCreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'users/signup.html'
+class SignupAPI(APIView):
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "User created"}, status=201)
+        return Response(serializer.errors, status=400)
 
-class UserProfile(LoginRequiredMixin, DetailView):
-    model = User
-    template_name = 'users/profile.html'
-    context_object_name = 'user_profile'
-    login_url = reverse_lazy('login') 
+
+# from django.shortcuts import render
+# from .forms import CustomUserCreationForm
+# from django.views.generic import CreateView, DetailView
+# from django.urls import reverse_lazy
+# from django.contrib.auth.mixins import LoginRequiredMixin 
+# from .models import User
+
+# class SignUpView(CreateView):
+#     form_class = CustomUserCreationForm
+#     success_url = reverse_lazy('login')
+#     template_name = 'users/signup.html'
+
+# class UserProfile(LoginRequiredMixin, DetailView):
+#     model = User
+#     template_name = 'users/profile.html'
+#     context_object_name = 'user_profile'
+#     login_url = reverse_lazy('login') 
     
-    def get_object(self):
-        return self.request.user
+#     def get_object(self):
+#         return self.request.user
     
