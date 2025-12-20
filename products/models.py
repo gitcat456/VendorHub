0c62a54cf170de4a03167e893a3538de621b2d51
gitcat456 <okothdenzel65@gmail.com>
2025-12-21 00:32:33 +0300

    refactors models for simplicity

--- products/models.py
+++ products/models.py
@@ -1,19 +1,6 @@
 from django.db import models
 from django.conf import settings  
 
-class VendorProfile(models.Model):
-    bss_name = models.CharField(max_length=50)
-    bss_location = models.CharField(max_length=100)  
-    profile_pic = models.ImageField(upload_to='vendor_profiles/')
-    vendor = models.OneToOneField(  
-        settings.AUTH_USER_MODEL, 
-        on_delete=models.CASCADE, 
-        related_name='vendor_profile'
-    )
-    
-    def __str__(self):
-        return self.bss_name
-
 class Category(models.Model):
     name = models.CharField(max_length=50)
     
@@ -29,23 +16,14 @@ class Product(models.Model):
         on_delete=models.CASCADE, 
         related_name='products'  
     )
-    vendor_profile = models.ForeignKey(
-        VendorProfile, 
+    vendor = models.ForeignKey(
+        settings.AUTH_USER_MODEL, 
         on_delete=models.CASCADE,
-        related_name='products'
+        related_name="vendor_products"
     )
+    image = models.ImageField(upload_to='product_images/', blank=True, null=True)  
     created_at = models.DateTimeField(auto_now_add=True)  
     
     def __str__(self):
         return self.name
 
-class ProductImage(models.Model):
-    image = models.ImageField(upload_to='product_images/')  
-    product = models.ForeignKey(
-        Product, 
-        on_delete=models.CASCADE, 
-        related_name='images'
-    )
-    
-    def __str__(self):
-        return f"Image for {self.product.name}"
