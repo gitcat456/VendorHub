d7dd077e516f8edde380d7fceccd73b1803bacfc
gitcat456 <okothdenzel65@gmail.com>
2025-12-20 22:04:23 +0300

    implements simple-jwt for authentication

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
-from .views import SignUpView, UserProfile
+from .views import SignupAPI
 from django.contrib.auth.views import LoginView, LogoutView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
-    path('', SignUpView.as_view(), name = 'signup'),
-    path('login/', LoginView.as_view(template_name = 'users/login.html'), name = 'login'),
+    path('', SignupAPI.as_view(), name = 'signup'),
+    path('login/', TokenObtainPairView.as_view(), name = 'login'),
     path('logout/', LogoutView.as_view(template_name = 'users/logout.html'), name = 'logout'),
-    path('profile/', UserProfile.as_view(), name='user_profile'),
+    path('profile/', TokenRefreshView.as_view(), name='user_profile'),
 ]
