5e2bad3d8c5fcf012c19eee720ef2cb5f2362c6b
gitcat456 <okothdenzel65@gmail.com>
2025-12-20 20:04:58 +0300

    adds location in the list_display

--- users/admin.py
+++ users/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import User
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ['id', 'username', 'email', 'phone_number', 'role']
+    list_display = ['id', 'username', 'email', 'phone_number', 'role','location']
     list_filter = ['role']
     search_fields = ['username', 'email']
     list_editable = ['role']  
