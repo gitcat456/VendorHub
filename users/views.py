e7aa10dd436f2d0424b0a2fc48d7ccd99e22e13c
gitcat456 <okothdenzel65@gmail.com>
2025-12-20 22:44:59 +0300

    creates profile API view

--- users/views.py
+++ users/views.py
@@ -1,7 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
-from .serializers import RegisterSerializer
+from .serializers import RegisterSerializer, ProfileSerializer
 
 class SignupAPI(APIView):
     def post(self, request):
@@ -9,6 +10,25 @@ class SignupAPI(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response({"message": "User created"}, status=201)
+        return Response(serializer.errors, status=400)
+
+
+class ProfileAPI(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        serializer = ProfileSerializer(request.user)
+        return Response(serializer.data)
+
+    def patch(self, request):
+        serializer = ProfileSerializer(
+            request.user,
+            data=request.data,
+            partial=True
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
         return Response(serializer.errors, status=400)
 
 
