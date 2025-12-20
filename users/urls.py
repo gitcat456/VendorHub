96a664c90e1721f9db460efd931cb6250373e731
gitcat456 <okothdenzel65@gmail.com>
2025-12-20 22:45:33 +0300

    reconfigures url to mach view classes

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import SignupAPI
-from django.contrib.auth.views import LoginView, LogoutView
+from .views import SignupAPI, ProfileAPI
+from django.contrib.auth.views import LogoutView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -10,5 +10,7 @@ urlpatterns = [
     path('', SignupAPI.as_view(), name = 'signup'),
     path('login/', TokenObtainPairView.as_view(), name = 'login'),
     path('logout/', LogoutView.as_view(template_name = 'users/logout.html'), name = 'logout'),
-    path('profile/', TokenRefreshView.as_view(), name='user_profile'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('profile/', ProfileAPI.as_view(), name="user_profile"),
+
 ]
