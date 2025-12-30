eae837d4f58e2795a1da80b69cb8891867ceaecf
gitcat456 <okothdenzel65@gmail.com>
2025-12-30 23:17:19 +0300

    url restructure to enhance harmony

--- vendorHub/urls.py
+++ vendorHub/urls.py
@@ -6,11 +6,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('users.urls')),
+    path('api/users/', include('users.urls')),
     path('api/', include('products.urls')),
-    path('interactions/', include('interactions.urls')),
-    path('subscriptions/', include('subscriptions.urls')),  
-    
+    path('api/interactions/', include('interactions.urls')),
+    path('api/subscriptions/', include('subscriptions.urls')),  
 ]
 
 #development only 
