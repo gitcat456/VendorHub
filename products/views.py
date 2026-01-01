69a201a0b126da4da4c39dc9948abf2a805d546a
gitcat456 <okothdenzel65@gmail.com>
2026-01-01 16:31:07 +0300

    Enhance ProductViewSet to allow read for all and require write for auth

--- products/views.py
+++ products/views.py
@@ -2,18 +2,41 @@ from django.shortcuts import render
 from rest_framework import viewsets 
 from .serializers import CategorySerializer, ProductSerializer
 from .models import Category, Product
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 class CategoryViewSet(viewsets.ModelViewSet):  
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
-class ProductViewSet(viewsets.ModelViewSet): 
-    queryset = Product.objects.all()
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Products.objects.all()
     serializer_class = ProductSerializer
-    
+
+    def get_permissions(self):
+        if self.action == 'list' or self.action == 'retrieve':
+            return [AllowAny()]
+        return [IsAuthenticated()]
+
     def get_queryset(self):
         queryset = Product.objects.all()
         vendor_id = self.request.query_params.get('vendor', None)
         if vendor_id:
             queryset = queryset.filter(vendor_id=vendor_id)
         return queryset
+
+    def perform_create(self, serializer):
+        serializer.save(vendor=self.request.user)
+
+#Initial code implementation
+
+# class ProductViewSet(viewsets.ModelViewSet): 
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
+#     permission_classes = [IsAuthenticated]
+    
+#     def get_queryset(self):
+#         queryset = Product.objects.all()
+#         vendor_id = self.request.query_params.get('vendor', None)
+#         if vendor_id:
+#             queryset = queryset.filter(vendor_id=vendor_id)
+#         return queryset
